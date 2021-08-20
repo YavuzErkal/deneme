ac9cb29b30faf17f22c0db50c12e2c1a9062a89c
YavuzErkal <erkalkeremyavuz@gmail.com>
2021-08-20 12:24:20 +0300

    Initial commit

---
+++ lib/counter_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class CounterProvider extends ChangeNotifier {
+
+  int _counter = 0;
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+
+}
